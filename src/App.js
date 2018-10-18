46c6426c2a13d9bc78545a7618d03726c669f7a4
aesthetism <zman97619@gmail.com>
2018-10-18 18:00:53 +0530

    +onDismiss

--- src/App.js
+++ src/App.js
@@ -1,21 +1,62 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
 
+const list = [
+  {
+  title: 'Road To React',
+  author: 'Jake',
+  pages: 200,
+  objectId:0
+  },
+  {
+  title: 'Sherlock Holmes',
+  author: 'Conan Doyle',
+  pages: 400,
+  objectId: 1
+  }
+]
 class App extends Component {
+  
+  constructor(props){
+    super(props);
+
+    this.state = {
+      list
+    };
+
+    this.onDismiss = this.onDismiss.bind(this);
+  }
+  
+  onDismiss(id){
+    const updatedList = this.state.list.filter(item => item.objectId !== id);
+    this.setState({list: updatedList});
+  }
+
   render() {
     return (
       <div className="App">
         <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
           <h1 className="App-title">Welcome Hackers</h1>
         </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+        <div className="App-intro">
+        {this.state.list.map(item=>{
+         return(
+         <div>{item.title}
+         <p>{item.author}</p>
+         <p>{item.pages}</p>
+         <p>{"id "+item.objectId}</p>
+         <span>
+           <button onClick={()=>this.onDismiss(item.objectId)}
+            type="button">
+           Dismiss
+           </button>
+         </span>
+         </div>
+         );}
+        )}
+        </div>
       </div>
     );
   }
 }
-
 export default App;
