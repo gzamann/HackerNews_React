31ecd424d254a4f100a667b3446b2ffee6f1311d
aesthetism <zman97619@gmail.com>
2018-10-19 17:07:49 +0530

    splitComponents

--- src/App.js
+++ src/App.js
@@ -36,43 +36,75 @@ class App extends Component {
   onSearchChange(event){
     this.setState({searchTerm: event.target.value});
   }
-
+  
   onDismiss(id){
     const updatedList = this.state.list.filter(item => item.objectId !== id);
     this.setState({list: updatedList});
   }
-
+  
   render() {
+    const {list,searchTerm} = this.state;
     return (
       <div className="App">
         
-        <form>
-          <input type="text"
-            onChange={this.onSearchChange}/>
-        </form>
+      <header className="App-header">
+        <h1 className="App-title">Welcome Hackers</h1>
+      </header>
 
-        <header className="App-header">
-          <h1 className="App-title">Welcome Hackers</h1>
-        </header>
-        <div className="App-intro">
-        {this.state.list.filter(isSearched(this.state.searchTerm)).map(item=>{
-         return(
-         <div>{item.title}
-         <p>{item.author}</p>
-         <p>{item.pages}</p>
-         <p>{"id "+item.objectId}</p>
-         <span>
-           <button onClick={() => this.onDismiss(item.objectId)}
-            type="button">
-           Dismiss
-           </button>
-         </span>
-         </div>
-         );}
-        )}
-        </div>
+        <Search 
+          value={searchTerm}
+          onChange={this.onSearchChange}
+        >
+        S
+        </Search>
+
+        <Table 
+          list = {list}
+          pattern = {searchTerm}
+          onDismiss = {this.onDismiss}
+        />
+
       </div>
     );
   }
 }
+
+class Search extends Component{
+  render(){
+    const {value, children, onChange} = this.props;
+    return(
+      <form>
+      {children} <input type="text"
+          value={value}
+          onChange={onChange}
+        />
+      </form>
+    );
+  }
+}
+
+class Table extends Component{
+  render(){
+    const {list, pattern, onDismiss} = this.props;
+    return(
+      <div className="table">
+        {list.filter(isSearched(pattern)).map(item =>
+          <div key={item.objectId}>
+            <span>
+              <a href={item.url}>{item.title}</a>
+            </span>
+            <span>{item.author}</span>
+            <span>{item.pages}</span>
+            <span>{"id: "+item.objectId}</span>
+            <button
+              onClick={()=>onDismiss(item.objectId)}
+              type="button">
+              Dismiss
+            </button>
+          </div>)}
+      </div>
+    );
+  }
+}
+
 export default App;
