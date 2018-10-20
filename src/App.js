2d0d87977a2e22f40cea603e6ad77c554d7355ee
aesthetism <zman97619@gmail.com>
2018-10-20 15:26:46 +0530

    API_integrated

--- src/App.js
+++ src/App.js
@@ -1,49 +1,60 @@
 import React, { Component } from 'react';
 import './App.css';
 
-const list = [
-  {
-  title: 'Road To React',
-  author: 'Jake',
-  pages: 200,
-  objectId:0
-  },
-  {
-  title: 'Sherlock Holmes',
-  author: 'Conan Doyle',
-  pages: 400,
-  objectId: 1
-  }
-]
+const DEFAULT_QUERY = 'elon';
+const PATH_BASE = 'https://hn.algolia.com/api/v1';
+const PATH_SEARCH = '/search';
+const PARAM_SEARCH = 'query=';
 
 const isSearched = searchTerm => item =>
-  item.title.toLowerCase().includes(searchTerm.toLowerCase());
+item.title.toLowerCase().includes(searchTerm.toLowerCase());
 
 class App extends Component {
   
   constructor(props){
     super(props);
-
+    
     this.state = {
-      list,
-      searchTerm: ""
+      result: null,
+      searchTerm: DEFAULT_QUERY,
     };
-
+    
+    this.setSearchTopStories= this.setSearchTopStories.bind(this);
     this.onSearchChange = this.onSearchChange.bind(this);
     this.onDismiss = this.onDismiss.bind(this);
   }
+  
+  setSearchTopStories(result){
+    this.setState({result});
+  }
+  
   
   onSearchChange(event){
     this.setState({searchTerm: event.target.value});
   }
   
   onDismiss(id){
-    const updatedList = this.state.list.filter(item => item.objectId !== id);
-    this.setState({list: updatedList});
+    const isNotId= item=>item.objectID!==id;
+    const updatedHits = this.state.result.hits.filter(isNotId);
+    
+    this.setState({
+      result: {...this.state.result, hits:updatedHits}
+    });
+  }
+  
+  componentDidMount() {
+    const { searchTerm } = this.state;
+    const url = `${PATH_BASE}${PATH_SEARCH}?${PARAM_SEARCH}${searchTerm}`;
+    fetch(url)
+    .then(response => response.json())
+    .then(result => this.setSearchTopStories(result))
+    .catch(error => error);
   }
   
   render() {
-    const {list,searchTerm} = this.state;
+    const {searchTerm, result} = this.state;
+    if(!result){return null;}
+    
     return (
       <div className="App">
         
@@ -57,15 +68,15 @@ class App extends Component {
         />
 
         <Table 
-          list = {list}
+          list = {result.hits}
           pattern = {searchTerm}
           onDismiss = {this.onDismiss}
         >
-        <div>
+        <div className="structtable">
           <div>Title</div>
           <div>Author</div>
-          <div>some</div>
-          <div>someelse</div>
+          <div>Points</div>
+          <div>comments</div>
         </div>
         </Table>
 
@@ -90,17 +101,17 @@ class Table extends Component{
       <div className="table">
       {children}
         {list.filter(isSearched(pattern)).map(item =>
-          <div key={item.objectId}>
+          <div className="tableitems" key={item.objectID}>
             <div>
               <a href={item.url}>{item.title}</a>
             </div>
             <div>{item.author}</div>
-            <div>{item.pages}</div>
-            <div>{"id: "+item.objectId}</div>
+            <div>{item.points}</div>
+            <div>{item.num_comments}</div>
             <div>
             <Button 
-              onClick={()=>onDismiss(item.objectId)}>
-              x
+              onClick={()=>onDismiss(item.objectID)}>
+              dismiss
             </Button>
             </div>
           </div>)}
